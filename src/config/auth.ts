dadc16fa828f45f1d482e2f30363b68402095024
ricardoamaralcrm <ricardo.crmservices@parceiro.elevaeducacao.com.br>
2022-04-29 16:15:56 -0300

    creating config file for token secrets/configuration, generating refresh_token when user authenticates

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "69c6e4a394b794ac45a4ca63761bff01",
+  expires_in_token: "15m",
+  secret_refresh_token: "17uoEtuihi6Lsg4hdedT7PUhF4FNgBPD2F",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30
+}
