fbf02f51b975b40b59d66e0a93fd6b576445d6e6
ricardoamaralcrm <ricardo.crmservices@parceiro.elevaeducacao.com.br>
2022-04-28 21:04:02 -0300

    creating DevolutionRentalController and fixing some logic on the devolution of the car

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -3,6 +3,7 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../IRentalsRepository";
 
 class RentalsRepositoryInMemory implements IRentalsRepository {
+  
   rentals: Rental[] = [];
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
@@ -13,19 +14,26 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
     return this.rentals.find(rental => rental.user_id === user_id && !rental.end_date);
   }
 
-  async create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+  async create({ user_id, car_id, expected_return_date, end_date, total, id }: ICreateRentalDTO): Promise<Rental> {
     const rental = new Rental();
 
     Object.assign(rental, {
+      id,
       user_id,
       car_id,
       expected_return_date,
-      start_date: new Date()
+      start_date: new Date(),
+      end_date,
+      total
     });
 
     this.rentals.push(rental);
 
     return rental;
+  }
+
+  async findById(id: string): Promise<Rental> {
+    return this.rentals.find(rental => rental.id === id);
   }
 }
 
