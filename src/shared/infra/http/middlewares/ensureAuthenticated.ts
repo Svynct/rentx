4cff4378391f9d8a735b2783a91c791544c80df6
ricardoamaralcrm <ricardo.crmservices@parceiro.elevaeducacao.com.br>
2022-04-29 17:23:00 -0300

    creating refresh token controller and route

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -4,13 +4,14 @@ import { verify } from "jsonwebtoken";
 import { AppError } from "@shared/errors/AppError";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 
 interface IPayload {
   sub: string;
 }
 
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
-  const { secret_token } = auth;
+  const { secret_refresh_token } = auth;
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
@@ -20,18 +21,18 @@ export async function ensureAuthenticated(request: Request, response: Response, 
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(token, secret_token) as IPayload;
+    const { sub: user_id } = verify(token, secret_refresh_token) as IPayload;
 
-    const usersRepository = new UsersRepository();
+    const usersTokensRepository = new UsersTokensRepository();
 
-    const user = await usersRepository.findById(user_id);
+    const user = await usersTokensRepository.findByUserIdAndRefreshToken(user_id, token);
 
     if (!user) {
       throw new AppError("User does not exist", 401);
     }
 
     request.user = {
-      id: user.id
+      id: user_id
     }
 
     next();
