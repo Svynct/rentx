8331d1b821a6efbf7292a0aa0a36bf172be79d80
ricardoamaralcrm <ricardo.crmservices@parceiro.elevaeducacao.com.br>
2022-04-29 21:58:32 -0300

    creating e-mail template for forgot my e-mail with handlebars

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,4 +1,6 @@
+import fs from "fs";
 import nodemailer, { Transporter } from "nodemailer";
+import handlebars from "handlebars";
 
 import { IMailProvider } from "../IMailProvider";
 
@@ -22,13 +24,18 @@ class EtherealMailProvider implements IMailProvider {
     .catch((err) => console.error(err));
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(to: string, subject: string, variables: any, path: string): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHtml = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: "Rentx <noreply@rentx.com.br>",
       subject,
-      text: body,
-      html: body
+      html: templateHtml
     });
 
     console.log("message sent: %s", message.messageId);
