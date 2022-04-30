6fd38b2ba0baeedc2ecc4d97c1420725f61fd51b
ricardoamaralcrm <ricardo.crmservices@parceiro.elevaeducacao.com.br>
2022-04-29 21:36:03 -0300

    creating mail provider

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -5,6 +5,7 @@ import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepositor
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -14,10 +15,12 @@ class SendForgotPasswordMailUseCase {
     @inject("UsersTokensRepository")
     private usersTokensRepository: IUsersTokensRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
-  ) {}
+    private dateProvider: IDateProvider,
+    @inject("EtherealMailProvider")
+    private mailProvider: IMailProvider
+  ) { }
 
-  async execute(email: string) {
+  async execute(email: string): Promise<void> {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
@@ -25,7 +28,7 @@ class SendForgotPasswordMailUseCase {
     }
 
     const refresh_token = uuidV4();
-    
+
     const expires_date = this.dateProvider.addHours(3);
 
     await this.usersTokensRepository.create({
@@ -33,6 +36,12 @@ class SendForgotPasswordMailUseCase {
       refresh_token,
       expires_date
     });
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de senha",
+      `O link para o reset é ${refresh_token}`
+    );
   }
 }
 
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,7 @@
 import { container } from 'tsyringe';
 
-import "@shared/container/providers";
+import "@shared/container/providers/DateProvider";
+import "@shared/container/providers/MailProvider";
 
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
