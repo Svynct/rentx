cfb0fe82bd82fbf25b3e7914f57b37f9922f9957
ricardoamaralcrm <ricardo.crmservices@parceiro.elevaeducacao.com.br>
2022-04-29 21:37:26 -0300

    adjusts on providers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,6 @@
 import { container } from 'tsyringe';
 
-import "@shared/container/providers/DateProvider";
-import "@shared/container/providers/MailProvider";
+import "@shared/container/providers";
 
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
