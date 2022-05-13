5bd7781acccb6e46e89bee0ea0960596a99bfa0f
ricardoamaralcrm <ricardo.crmservices@parceiro.elevaeducacao.com.br>
2022-05-12 21:56:36 -0300

    adding rate limiter and redis

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -11,17 +11,16 @@ import { AppError } from "@shared/errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "../../../swagger.json";
 import upload from "@config/upload";
+import rateLimiter from "./middlewares/rateLimiter";
 
 createConnection();
 const app = express();
 
+app.use(rateLimiter);
 app.use(express.json());
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
-
 app.use(cors());
 app.use(router);
 
